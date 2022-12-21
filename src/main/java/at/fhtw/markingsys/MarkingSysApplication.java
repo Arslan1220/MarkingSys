f60800a8cd316fa3adb62ac9754d900bb2820968
Arslan1220 <55580858+Arslan1220@users.noreply.github.com>
2022-12-21 18:14:48 +0100

    Start

---
+++ src/main/java/at/fhtw/markingsys/MarkingSysApplication.java
@@ -0,0 +1,13 @@
+package at.fhtw.markingsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarkingSysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarkingSysApplication.class, args);
+    }
+
+}
---
+++ src/test/java/at/fhtw/markingsys/MarkingSysApplicationTests.java
@@ -0,0 +1,13 @@
+package at.fhtw.markingsys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarkingSysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
